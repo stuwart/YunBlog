8fa4a8de5e05edb9314732112b1ddd89cd9f7e01
huangyun <hy020316@126.com>
2024-02-02 01:17:00 +0800

    样式更新

--- frontend/src/components/TagList.vue
+++ frontend/src/components/TagList.vue
@@ -1,38 +1,71 @@
 <template>
   <div class="left">
-    <div v-for = "item in tags" :key="item.id">
-        <a href="#">{{ item.name }}({{item.num }})</a>
+    <div class="title">标签</div>
+    <div v-for="item in tags" :key="item.id">
+      <div class="tagname">
+        <a href="#">{{ item.name }}({{ item.num }})</a>
+      </div>
     </div>
+    <el-divider direction="vertical" />
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const tags = [
-    {
-        name : '1111',
-        url : 'http:111',
-        num: 4
-    },
-    {
-        name : '1111',
-        url : 'http:111',
-        num:3
-    },
-    {
-        name : '1111',
-        url : 'http:111',
-        num:2
-    },
-    {
-        name : '1111',
-        url : 'http:111',
-        num:6
-    }
+  {
+    name: "1111",
+    url: "http:111",
+    num: 4,
+  },
+  {
+    name: "1111",
+    url: "http:111",
+    num: 3,
+  },
+  {
+    name: "1111",
+    url: "http:111",
+    num: 2,
+  },
+  {
+    name: "1111",
+    url: "http:111",
+    num: 6,
+  },
 ];
+
+let len = tags.length;
 </script>
 
 <style lang="scss" scoped>
-.left{
+.title {
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
+}
+.left {
+  position: absolute;
+  left: 20%;
+  margin-top: 20px;
+}
 
+.tagname {
+  margin: 16px;
+  font-size: 16px;
+}
+
+a {
+  text-decoration-line: none;
+  transition: all 0.3s ease;
+  display: inline-block;
+}
+a:hover {
+  // 变大
+  font-size: 20px;
+  background-color: grey;
+  padding: 5px; /* 添加一些内边距以便观察到阴影效果 */
+  
 }
 </style>
--- frontend/src/views/Tag.vue
+++ frontend/src/views/Tag.vue
@@ -1,11 +1,15 @@
 <template>
   <Header></Header>
-  <TagList></TagList>
+  <div class="main">
+    <TagList></TagList>
+    <TagArticle></TagArticle>
+  </div>
 </template>
   
 <script setup>
 import Header from "@/components/Header.vue";
 import TagList from "@/components/TagList.vue";
+import TagArticle from "@/components/TagArticle.vue";
 </script>
   
 <style lang="scss" scoped>
